df2703969e68c7a446d87a0dce1a8c88c5a83c3b
Jonas Ababio <jonasagesmilly@outlook.com>
2025-01-04 21:06:00 +0000

    add launch configuration and implement substitution functionality in main.go

--- main.go
+++ main.go
@@ -1,2 +1,40 @@
 package main
 
+import (
+	"flag"
+	"io"
+	"os"
+	"strings"
+)
+
+func main() {
+    flag.Parse()
+	substitution := flag.Args()[0]
+    filename := flag.Args()[1]
+    if filename == "" {
+        flag.PrintDefaults()
+        return
+    }
+    file, err := os.Open(filename)
+    if err != nil {
+        panic(err)
+    }
+    defer file.Close()
+    content, err := io.ReadAll(file)
+    if err != nil {
+        panic(err)
+    }
+    result := substitute(content, substitution)
+    println(string(result))
+}
+
+func substitute(content []byte, substitution string) (result []byte) {
+    substitution = strings.TrimPrefix(substitution, "s/")
+    substitution = strings.TrimSuffix(substitution, "/g")
+    parts := strings.Split(substitution, "/")
+    if len(parts) != 2 {
+        panic("invalid substitution")
+    }
+    result = []byte(strings.ReplaceAll(string(content), parts[0], parts[1]))
+    return result
+}
